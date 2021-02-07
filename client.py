a7b0b804e025e6c3f20c44e0a5df5941da183a8f
Alex Leung <lygg725@163.com>
2021-02-07 18:25:35 +0800

    update

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ sk.connect(('127.0.0.1', 60000))
 # 0xFFFF FFFF 0000 0064 0000 1111
 headcode = str(0xFFFFFFFF0000006400021111)
 print(headcode)
-header = b'\xff\xff\xff\xff' + b'\x00\x00' + b'\x10\x10'
+header = b'\xff\xff\xff\xff' + b'\x00\x00' + b'\x00\x64' + b'\x00\x02' + b'\x10\x10'
 # print(type(header))
 body = bytes('{"save":0,"patientid":"123456","caseid":"12345678"}', encoding='utf-8')
 content = header + body
